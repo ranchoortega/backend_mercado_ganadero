4791dde474dd9c56f69774c165f09e1f62d433b2
Alexander090409 <jonathanalexanderramirezortega@gmail.com>
2024-08-28 19:31:31 -0600

    Falta aves y chivos

--- application/models/ClienteModel.php
+++ application/models/ClienteModel.php
@@ -14,11 +14,37 @@ class ClienteModel extends CI_Model
     public function getAnimales($tipo)
     {
 
-        $this->db->select("F.url,A.*");
+        $this->db->select("F.url,A.*,U.phone");
         $this->db->from('files F');
         $this->db->join('descripcion_animal A', 'F.id_descripcion = A.id_descripcion');
+        $this->db->join(' Usuarios U', ' = A.id_usuario = U.id_user');
         $this->db->where('A.tipo', $tipo);
         $this->db->group_by('F.id_descripcion'); // Agrupa por id_descripcion para evitar duplicados
+        $this->db->order_by('A.id_usuario', 'ASC');
+
+        $this->db->order_by('F.id_descripcion', 'DESC');
+        
+
+
+
+        $query = $this->db->get();
+        if ($query->num_rows() > 0) {
+            return $query->result();
+        } else {
+            return false;
+        }
+
+    }
+    public function getAnimalesNew($tipo)
+    {
+
+        $this->db->select("F.url,A.*,U.phone");
+        $this->db->from('files F');
+        $this->db->join('descripcion_animal A', 'F.id_descripcion = A.id_descripcion');
+        $this->db->join(' Usuarios U', ' = A.id_usuario = U.id_user');
+        $this->db->where('A.tipo', $tipo);
+        $this->db->group_by('F.id_descripcion'); // Agrupa por id_descripcion para evitar duplicados
+
         $this->db->order_by('F.id_descripcion', 'DESC');
         
 
